5db1b57995e96cec156985443857a47ed2ecaf3c
Alviannn <alvianndq@gmail.com>
2023-06-02 23:09:20 +0700

    feat: add entry program

---
+++ internal/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"go-boilerplate/internal/helpers"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
+	"go.uber.org/dig"
+)
+
+func ProvideDIContainer() (container *dig.Container, err error) {
+	err = godotenv.Load()
+	if err != nil {
+		return
+	}
+
+	container = dig.New()
+	err = helpers.MultiProvideDI(container, []any{
+		echo.New,
+	})
+
+	return
+}
+
+func main() {
+	container, err := ProvideDIContainer()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	container.Invoke(func(e *echo.Echo) {
+		e.Start(fmt.Sprintf(":%s", os.Getenv("PORT")))
+	})
+}
