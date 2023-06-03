0bbb4a1e516d81c9c9556ae0f92e674da98f3d19
Alviannn <alvianndq@gmail.com>
2023-06-04 00:16:16 +0700

    feat(model): add user model

---
+++ internal/models/users_model.go
@@ -0,0 +1,10 @@
+package models
+
+type User struct {
+	BaseModel
+
+	Username string `json:"username"`
+	FullName string `json:"fullName"`
+	Email    string `json:"email"`
+	Password string `json:"-"` // don't allow password to ever be exported.
+}
