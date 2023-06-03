41181b1594e3eac06a1381e9d1da93dd3d85aa4a
Alviannn <alvianndq@gmail.com>
2023-06-03 19:57:18 +0700

    refactor: move pkg database to inside internal

--- internal/pkg/databases/postgres_database.go
+++ internal/pkg/databases/postgres_database.go
@@ -9,16 +9,19 @@ import (
 	"gorm.io/gorm/logger"
 )
 
-func NewPostgresDB() (*gorm.DB, error) {
-	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s TimeZone=%s",
+func NewPostgresDB() (db *gorm.DB, err error) {
+	dsn := fmt.Sprintf("host=%s port=%s dbname=%s user=%s password=%s TimeZone=%s",
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
 		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_PASS"),
 		os.Getenv("DB_TIMEZONE"),
 	)
 
-	return gorm.Open(postgres.Open(dsn), &gorm.Config{
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Error),
 	})
+
+	return
 }
