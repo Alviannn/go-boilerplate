44ea96b0f91383fa6788ef71dd2c0a7cb9e87306
Alviannn <alvianndq@gmail.com>
2023-06-04 23:38:40 +0700

    feat(users): use standard response and error response

--- internal/domains/users/getuser/handler.go
+++ internal/domains/users/getuser/handler.go
@@ -2,6 +2,7 @@ package getuser
 
 import (
 	"go-boilerplate/internal/dtos"
+	"go-boilerplate/pkg/responses"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -17,14 +18,20 @@ func NewHandler(service Service) Handler {
 
 func (h Handler) Handle(c echo.Context) (err error) {
 	var params dtos.GetUserReq
+
 	if err = c.Bind(&params); err != nil {
+		err = responses.NewError().
+			WithCode(http.StatusBadRequest).
+			WithError(err).
+			WithMessage("Failed to bind parameters")
+
 		return
 	}
 
 	user, err := h.Service.GetUser(params)
-	if err != nil {
-		return
-	}
-
-	return c.JSON(http.StatusOK, user)
+	return responses.New().
+		WithData(user).
+		WithError(err).
+		WithSuccessCode(http.StatusOK).
+		Send(c)
 }
--- internal/domains/users/getuser/service.go
+++ internal/domains/users/getuser/service.go
@@ -1,8 +1,14 @@
 package getuser
 
 import (
+	"errors"
+	"fmt"
 	"go-boilerplate/internal/dtos"
 	"go-boilerplate/internal/models"
+	"go-boilerplate/pkg/responses"
+	"net/http"
+
+	"gorm.io/gorm"
 )
 
 type serviceImpl struct {
@@ -14,5 +20,22 @@ func NewService(repo Repository) Service {
 }
 
 func (s *serviceImpl) GetUser(params dtos.GetUserReq) (user models.User, err error) {
-	return s.Repository.GetUser(params.UserID)
+	user, err = s.Repository.GetUser(params.UserID)
+
+	if err != nil {
+		newErr := responses.NewError().
+			WithError(err).
+			WithCode(http.StatusInternalServerError).
+			WithMessage(fmt.Sprintf("Cannot fetch user with ID %s", params.UserID))
+
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			newErr.
+				WithCode(http.StatusNotFound).
+				WithMessage(fmt.Sprintf("Cannot find user with ID %s", params.UserID))
+		}
+
+		err = newErr
+	}
+
+	return
 }
