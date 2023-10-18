97ee59495600d1649522fee42c737fc678ee38dc
Alviannn <alvianndq@gmail.com>
2023-10-18 22:31:49 +0700

    feat(helpers): provide only clone request body

--- pkg/helpers/http_helper.go
+++ pkg/helpers/http_helper.go
@@ -2,35 +2,30 @@ package helpers
 
 import (
 	"bytes"
+	"go-boilerplate/pkg/responses"
 	"io"
 	"net/http"
 )
 
 type httpHelper struct{}
 
-func (h *httpHelper) CloneBody(req *http.Request) (clonedBody []byte, err error) {
+func (h *httpHelper) CloneRequestBody(req *http.Request) (clonedBody []byte, err error) {
+	isHasNoBody := (req.Body == http.NoBody || req.Body == nil)
+	if isHasNoBody {
+		return
+	}
 	defer req.Body.Close()
 
 	clonedBody, err = io.ReadAll(req.Body)
 	if err != nil {
+		err = responses.NewError().
+			WithSourceError(err).
+			WithCode(http.StatusInternalServerError).
+			WithMessage("Failed to read request body.")
 		return
 	}
 
 	bodyReader := bytes.NewReader(clonedBody)
 	req.Body = io.NopCloser(bodyReader)
-
-	return
-}
-
-func (h *httpHelper) CloneRequest(req *http.Request) (clonedReq *http.Request, err error) {
-	clonedBody, err := h.CloneBody(req)
-	if err != nil {
-		return
-	}
-
-	ctx := req.Context()
-	clonedReq = req.Clone(ctx)
-	clonedReq.Body = io.NopCloser(bytes.NewReader(clonedBody))
-
 	return
 }
