e63f78ec8b405c2013a5f168aebd1617a94b7721
Alviannn <alvianndq@gmail.com>
2023-10-19 00:33:00 +0700

    feat(logger): use error response to marshal custom error

--- pkg/logger/error_handler.go
+++ pkg/logger/error_handler.go
@@ -2,21 +2,11 @@ package logger
 
 import "go-boilerplate/pkg/responses"
 
-type CustomErrorInLog struct {
-	Error       string   `json:"error"`
-	SourceError string   `json:"sourceError"`
-	Stack       []string `json:"stack"`
-}
-
 func HandleCustomError(err error) any {
 	customError, ok := err.(*responses.CustomError)
 	if !ok {
-		return err.Error()
+		return err
 	}
 
-	return CustomErrorInLog{
-		Error:       customError.Error(),
-		SourceError: customError.GetWorkingError().Error(),
-		Stack:       customError.GetStackTrace(),
-	}
+	return customError.BuildResponse()
 }
