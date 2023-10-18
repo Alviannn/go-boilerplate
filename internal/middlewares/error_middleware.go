dfeaf6345fd1355d79d2fdf13e2044abc6fd9e67
Alviannn <alvianndq@gmail.com>
2023-10-19 00:33:24 +0700

    feat(middleware): log unhandled errors

--- internal/middlewares/error_middleware.go
+++ internal/middlewares/error_middleware.go
@@ -4,12 +4,14 @@ import (
 	"go-boilerplate/pkg/responses"
 
 	"github.com/labstack/echo/v4"
+	"github.com/rs/zerolog/log"
 )
 
 func CustomErrorHandler() echo.HTTPErrorHandler {
 	return func(err error, c echo.Context) {
-		customErr := responses.NewError().
-			WithSourceError(err)
+		customErr := responses.NewError().WithSourceError(err)
+
+		log.Error().Err(customErr).Msg("Unhandled error")
 
 		responses.New().
 			WithError(customErr).
