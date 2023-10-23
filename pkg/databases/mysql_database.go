09d0d132b01ccf9e3866c690e54bac31ddbf463b
Alviannn <alvianndq@gmail.com>
2023-10-24 00:05:24 +0700

    fix(db): fix new mysql client

--- pkg/databases/mysql_database.go
+++ pkg/databases/mysql_database.go
@@ -13,13 +13,12 @@ import (
 )
 
 func NewMySQLDB() (db *gorm.DB, err error) {
-	dsn := fmt.Sprintf("host=%s port=%s dbname=%s user=%s password=%s TimeZone=%s",
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=UTC",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASS"),
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_PORT"),
 		os.Getenv("DB_NAME"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASS"),
-		os.Getenv("DB_TIMEZONE"),
 	)
 
 	return gorm.Open(mysql.Open(dsn), &gorm.Config{
