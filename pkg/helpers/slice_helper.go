5e0aa70b97a17a6b4608b26672bd1fc012f1b191
Alviannn <alvianndq@gmail.com>
2024-10-24 22:33:20 +0700

    feat(helper): add is in with func

--- pkg/helpers/slice_helper.go
+++ pkg/helpers/slice_helper.go
@@ -2,12 +2,21 @@ package helpers
 
 type sliceHelper[T comparable] struct{}
 
-func (sliceHelper[T]) IsIn(slice []T, itemToFind T) bool {
+func (h sliceHelper[T]) IsInWithFunc(slice []T, itemToFind T, equalFunc func(T, T) bool) bool {
 	for _, item := range slice {
-		if itemToFind == item {
+		if equalFunc(itemToFind, item) {
 			return true
 		}
 	}
-
 	return false
+}
+
+func (h sliceHelper[T]) IsIn(slice []T, itemToFind T) bool {
+	return h.IsInWithFunc(
+		slice,
+		itemToFind,
+		func(a T, b T) bool {
+			return a == b
+		},
+	)
 }
