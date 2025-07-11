9be0da4e01f84b02ce1aa330ed9279f70bd193c3
Alviannn <alvianndq@gmail.com>
2025-07-11 20:09:52 +0700

    refactor(customtypes): refactor csv type

--- pkg/customtypes/csv.go
+++ pkg/customtypes/csv.go
@@ -22,30 +22,31 @@ type CSV struct {
 }
 
 func (ct *CSV) ToSlice() []string {
-	splitList := strings.Split(ct.Source, ct.GetComma())
+	currentComma := ct.Comma
+	if currentComma == "" {
+		currentComma = DefaultComma
+	}
+
+	splitList := strings.Split(ct.Source, currentComma)
 	if len(splitList) == 1 && splitList[0] == "" {
 		return []string{}
 	}
 	return splitList
 }
 
-func (ct *CSV) GetComma() string {
-	if ct.Comma == "" {
-		ct.Comma = DefaultComma
-	}
-	return ct.Comma
-}
-
+// UnmarshalJSON implements the json.Unmarshaler interface.
 func (ct *CSV) UnmarshalJSON(buf []byte) error {
 	ct.Source = string(buf)
 	return nil
 }
 
+// UnmarshalText implements the encoding.TextUnmarshaler interface.
 func (ct *CSV) UnmarshalText(text []byte) error {
 	ct.Source = string(text)
 	return nil
 }
 
+// UnmarshalParam implements the echo.BindUnmarshaler interface.
 func (ct *CSV) UnmarshalParam(param string) error {
 	ct.Source = param
 	return nil
