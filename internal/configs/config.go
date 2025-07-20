639cfe1f3630b12b155bffd8ffba34628aca076f
Alviannn <alvianndq@gmail.com>
2025-07-20 17:26:17 +0700

    feat(config): add default port

--- internal/configs/config.go
+++ internal/configs/config.go
@@ -6,18 +6,26 @@ import (
 	"go-boilerplate/internal/constants"
 	"go-boilerplate/pkg/customerror"
 	"go-boilerplate/pkg/customvalidator"
+	"os"
 
 	"github.com/caarlos0/env/v11"
 	"github.com/joho/godotenv"
 )
 
 type Config struct {
-	Port               int                `env:"PORT" validate:"required"`
+	Port               int                `env:"PORT"`
 	Environment        constants.EnvValue `env:"ENVIRONMENT" validate:"oneof=production development"`
 	CORSAllowedOrigins []string           `env:"CORS_ALLOWED_ORIGINS" envSeparator:","`
 	LogsDir            string             `env:"LOGS_DIR" validate:"required"`
 
 	MySQL MySQLConfig `validate:"required"`
+}
+
+func (c *Config) Validate() (err error) {
+	if c.Port == 0 {
+		c.Port = 5000
+	}
+	return
 }
 
 var defaultConfig Config
