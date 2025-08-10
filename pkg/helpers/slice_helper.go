4e4cd7806b82c267d9cb989441f06bcc5f4f2916
Alviannn <alvianndq@gmail.com>
2025-08-10 17:31:23 +0700

    feat(helper): remove unused slice helper

--- pkg/helpers/slice_helper.go
+++ pkg/helpers/slice_helper.go
@@ -1,23 +1,5 @@
 package helpers
 
-import "slices"
-
-type SliceMatchFunc[T comparable] func(current T) bool
-
-func SliceFindFunc[T comparable](slice []T, matchFunc SliceMatchFunc[T]) *T {
-	idx := slices.IndexFunc(slice, matchFunc)
-	if idx == -1 {
-		return nil
-	}
-	return &slice[idx]
-}
-
-func SliceFind[T comparable](slice []T, itemToFind T) *T {
-	return SliceFindFunc(slice, func(actual T) bool {
-		return itemToFind == actual
-	})
-}
-
 func SliceMap[Before any, After any](slice []Before, mapFunc func(Before) After) (newList []After) {
 	newList = make([]After, len(slice))
 	for i, v := range slice {
