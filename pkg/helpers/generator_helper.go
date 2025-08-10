ef51a155d191a74f881259ffd1d467c3494916b9
Alviannn <alvianndq@gmail.com>
2025-08-10 21:15:17 +0700

    refactor(helper): refactor generator helper

--- pkg/helpers/generator_helper.go
+++ pkg/helpers/generator_helper.go
@@ -2,7 +2,6 @@ package helpers
 
 import (
 	"math/rand/v2"
-	"time"
 )
 
 type (
@@ -11,17 +10,11 @@ type (
 		WithUpperCase bool
 		IncludeNumber bool
 		IncludeSymbol bool
-		Seed          uint64
-	}
-
-	GeneratorRandomBoolParam struct {
-		Seed uint64
 	}
 
 	GeneratorRandomIntParam struct {
-		Min  int
-		Max  int
-		Seed uint64
+		Min int
+		Max int
 	}
 )
 
@@ -33,56 +26,41 @@ var (
 )
 
 func GeneratorRandomString(param GeneratorRandomStringParam) string {
-	if param.Seed == 0 {
-		param.Seed = uint64(time.Now().UnixNano())
-	}
-
 	var (
-		randomer         = rand.New(rand.NewPCG(param.Seed, param.Seed))
 		generateFuncList = []func() string{
 			func() string {
-				return string(charsetLower[randomer.IntN(len(charsetLower))-1])
+				return string(charsetLower[rand.IntN(len(charsetLower))-1])
 			},
 		}
 	)
 
 	if param.WithUpperCase {
 		generateFuncList = append(generateFuncList, func() string {
-			return string(charsetUpper[randomer.IntN(len(charsetUpper))-1])
+			return string(charsetUpper[rand.IntN(len(charsetUpper))-1])
 		})
 	}
 	if param.IncludeNumber {
 		generateFuncList = append(generateFuncList, func() string {
-			return string(charsetNumber[randomer.IntN(len(charsetNumber))-1])
+			return string(charsetNumber[rand.IntN(len(charsetNumber))-1])
 		})
 	}
 	if param.IncludeSymbol {
 		generateFuncList = append(generateFuncList, func() string {
-			return string(charsetSymbol[randomer.IntN(len(charsetSymbol))-1])
+			return string(charsetSymbol[rand.IntN(len(charsetSymbol))-1])
 		})
 	}
 
 	var result string
 	for i := 0; i < param.Length; i++ {
-		result += generateFuncList[randomer.IntN(len(generateFuncList))]()
+		result += generateFuncList[rand.IntN(len(generateFuncList))]()
 	}
 	return result
 }
 
-func GeneratorRandomBool(param GeneratorRandomBoolParam) bool {
-	if param.Seed == 0 {
-		param.Seed = uint64(time.Now().UnixNano())
-	}
-
-	randomer := rand.New(rand.NewPCG(param.Seed, param.Seed))
-	return randomer.IntN(2) == 1
+func GeneratorRandomBool() bool {
+	return rand.IntN(2) == 1
 }
 
 func GeneratorRandomInt(param GeneratorRandomIntParam) int {
-	if param.Seed == 0 {
-		param.Seed = uint64(time.Now().UnixNano())
-	}
-
-	randomer := rand.New(rand.NewPCG(param.Seed, param.Seed))
-	return randomer.IntN(param.Max-param.Min+1) + param.Min
+	return rand.IntN(param.Max-param.Min+1) + param.Min
 }
