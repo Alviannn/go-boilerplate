a40149bcd7397059597a8e6fcfae18ce1bc1b3cc
Alviannn <alvianndq@gmail.com>
2026-04-05 17:38:29 +0700

    feat(helper): add slice dedup func

--- pkg/helpers/slice_helper.go
+++ pkg/helpers/slice_helper.go
@@ -7,3 +7,29 @@ func SliceMap[Before any, After any](slice []Before, mapFunc func(Before) After)
 	}
 	return
 }
+
+func SliceDeduplicateFunc[T any, K comparable](oldList []T, keyFunc func(T) K) []T {
+	var (
+		seen   = make(map[K]struct{}, len(oldList))
+		result = make([]T, 0)
+	)
+
+	for _, v := range oldList {
+		key := keyFunc(v)
+
+		if _, exists := seen[key]; exists {
+			continue
+		}
+
+		seen[key] = struct{}{}
+		result = append(result, v)
+	}
+
+	return result
+}
+
+func SliceDeduplicate[T comparable](oldList []T) []T {
+	return SliceDeduplicateFunc(oldList, func(key T) T {
+		return key
+	})
+}
